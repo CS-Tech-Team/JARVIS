0a4a02a0a748ed6332162b734b71be2b517628ef
Batuhan Ipek <batuhanipek2019@gmail.com>
2024-05-19 21:04:04 +0300

    modulized speech recognition

---
+++ SpeechToText/__init__.py
@@ -0,0 +1,43 @@
+import threading
+import queue
+from .whisper_speech_recognition import LiveWhisper
+
+class LiveWhisperListener:
+    def __init__(self, model_name="medium.en", device_name="default", speech_threshold=0.5):
+        self.asr = LiveWhisper(model_name, speech_threshold=speech_threshold, device_name=device_name)
+
+    def start(self, q):
+        self.asr.start()
+        try:
+            while True:
+                text, sample_length, inference_time = self.asr.get_last_text()
+                if text:
+                    q.put(text)
+        except KeyboardInterrupt:
+            self.asr.stop()
+            exit()
+
+def speech_listener(q, model_name="medium.en", device_name="default", speech_threshold=0.5):
+    listener = LiveWhisperListener(model_name=model_name, device_name=device_name, speech_threshold=speech_threshold)
+    listener.start(q)
+
+def speech_listener_thread(q):
+    listener_thread = threading.Thread(target=speech_listener, args=(q,))
+    listener_thread.daemon = True
+    listener_thread.start()
+
+if __name__ == "__main__":
+    newq = queue.Queue()
+    
+    # listener_thread = threading.Thread(target=speech_listener, args=(newq,))
+    # listener_thread.daemon = True
+    # listener_thread.start()
+    
+    speech_listener_thread(newq)
+    
+    while True:
+        try:
+            text = newq.get()  # Wait for new text with a timeout
+            print(f"Received text: {text}")
+        except queue.Empty:
+            print("No new text received.")
---
+++ main.py
@@ -0,0 +1,30 @@
+import queue
+import threading
+import os
+
+from SpeechToText import speech_listener_thread
+
+speech_results_q = queue.Queue()
+speech_listener_thread(speech_results_q)
+
+while True:
+    try:
+    
+        for thread in threading.enumerate():
+            print(f"Thread Name: {thread.name}, Thread ID: {thread.ident}")
+        
+    
+    
+        text = speech_results_q.get()  # Wait for new text with a timeout
+        
+    
+    
+        if "close" in text:
+            os._exit(-1)
+        
+        print(f"User: {text}")
+    
+    
+    
+    except queue.Empty:
+        print("No new voice received.")
